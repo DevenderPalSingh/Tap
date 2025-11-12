399bed961109aacf4dd3e61551ccd8a5cd30019e
DevenderPalSingh <devenderpal.singhdsr@gmail.com>
2025-11-13 01:01:53 +0530

    Here it is 3rd day

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("explore-btn").addEventListener("click", () => {
+  window.scrollTo({
+    top: document.querySelector(".features").offsetTop,
+    beahviour: "smooth",
+  });
+});
+
+document.getElementById("shop-btn").addEventListener("click", () => {
+  alert("Redirecting to shop page... (feature coming soon!");
+});
+// Comment Test Is Done Over Here....................................
